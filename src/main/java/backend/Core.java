d87e87db4778495fb9a8a2b214a311ace70e68d8
Justin Bell <justin.bell@mail.mcgill.ca>
2018-01-23 15:27:48 -0500

    Remove useless files

--- src/main/java/backend/Core.java
+++ src/main/java/backend/Core.java
@@ -1,26 +1,19 @@
 package backend;
 
-import database.DatabaseException;
-import database.FlatFileDb;
-import database.IDatabase;
 import java.io.IOException;
-import org.eclipse.jgit.api.errors.GitAPIException;
 
 public class Core {
   private Repo repo;
-  private IDatabase db;
   private int step;
 
   /**
    * Constructor for the backend core.
    * 
    * @param repo wrapper for interacting with git repository
-   * @param db database for storing relationships between commits and tutorial steps
    */
-  public Core(Repo repo, IDatabase db) {
+  public Core(Repo repo) {
     this.step = 0;
     this.repo = repo;
-    this.db = db;
   }
 
   /**
@@ -32,8 +25,7 @@ public class Core {
    */
   public static Core initCore(String path) throws IOException {
     Repo repo = Repo.initRepo(path);
-    IDatabase db = FlatFileDb.initDb(path);
-    return new Core(repo, db);
+    return new Core(repo);
   }
 
   /**
@@ -45,17 +37,5 @@ public class Core {
   public static Core initCore() throws IOException {
     String path = System.getProperty("user.dir");
     return initCore(path);
-  }
-
-  /**
-   * commits all current changes to active tutorial step. Changes are added as a commit and the hash
-   * is stored in the database.
-   *
-   * @throws GitAPIException issues with git
-   * @throws DatabaseException database processing issues
-   */
-  public void updateStep() throws GitAPIException, DatabaseException {
-    String hash = this.repo.commitAll();
-    this.db.addSha(this.step, hash);
   }
 }
