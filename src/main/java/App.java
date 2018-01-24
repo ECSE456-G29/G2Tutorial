fb0774f1838552fe450351325e6e1330442c8962
Justin Bell <justin.bell@mail.mcgill.ca>
2018-01-24 14:16:44 -0500

    Add init as a command
    resolves #5

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import backend.Core;
+import java.io.IOException;
 import parse.GreetParser;
 
 public class App {
@@ -11,11 +13,27 @@ public class App {
    * @param  args commandline arguments
    */
   public static void main(String[] args) {
-    if (args.length > 0 && args[0].equals("greet")) {
+    if (args.length < 1) {
+      System.out.println("Available commands:\n\tgreet");
+      System.exit(1);
+    }
+
+    if (args[0].equals("greet")) {
+      // Greet just exists as a template for parsing
       GreetParser p = new GreetParser(args);
       p.parse();
+    } else if (args[0].equals("init")) {
+      // Initializes an empty git repo
+      try {
+        Core.initCore();
+      } catch (IOException e) {
+        System.err.println("Could not init g2t");
+        e.printStackTrace();
+      }
+      System.exit(0);
     } else {
-      System.out.println("Available commands:\n\tgreet");
+      System.out.println("Available commands:\n"
+          + "\tgreet, init");
       System.exit(1);
     }
   }
