fa40010e9ea6289d2fbfcbac1ffdd6d0b9417cd4
Justin Bell <justin.bell@mail.mcgill.ca>
2018-02-18 18:36:15 -0500

    Skeleton for #11

---
+++ src/main/java/backend/Doc.java
@@ -0,0 +1,17 @@
+package backend;
+
+import java.util.Set;
+
+public class Doc {
+
+  /**
+   * Parses the specified asciidoc step file and returns the set of files that are mentioned as
+   * changed.
+   * @param fname name of step asciidoc
+   * @return set of filenames that are mentioned as changed in the asciidoc step file
+   */
+  public static Set<String> filesChanged(String fname) {
+    // TODO: Implement this method
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+}
