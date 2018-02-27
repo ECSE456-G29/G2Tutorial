f2f40387a46dbd65960cde76aedd51fc82a5af45
Ryan-Chalmers <ryan.chalmers8@gmail.com>
2018-02-27 15:50:19 -0500

    began work on asciidoc parsing in DOC.java

--- src/main/java/backend/Doc.java
+++ src/main/java/backend/Doc.java
@@ -1,8 +1,11 @@
 package backend;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Set;
 
 public class Doc {
+
 
   /**
    * Parses the specified asciidoc step file and returns the set of files that are mentioned as
@@ -12,6 +15,19 @@ public class Doc {
    */
   public static Set<String> filesChanged(String fname) {
     // TODO: Implement this method
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(fname));
+      String line;
+      while((line = reader.readLine()) != null){
+        /*TODO: implement the following
+        Check for the WARNING: keyword at the beginning of the line. If the warning found then parse the file path
+        out of the line and add it the set
+         */
+      }
+    } catch (Exception e) {
+      System.err.format("Exception occurred trying to read '%s'.", fname);
+      e.printStackTrace();
+    }
     throw new UnsupportedOperationException("Not implemented yet");
   }
 }
