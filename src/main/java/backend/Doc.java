27bb7850198c73a8112c8c0cd21be63add75baf8
Ryan-Chalmers <ryan.chalmers8@gmail.com>
2018-02-28 11:35:03 -0500

    finished asciidoc file change parser for warning of the type "WARNING: /abs/file/path Has been changed!"

--- src/main/java/backend/Doc.java
+++ src/main/java/backend/Doc.java
@@ -2,10 +2,12 @@ package backend;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Doc {
 
+  private final static String token = "WARNING:";
 
   /**
    * Parses the specified asciidoc step file and returns the set of files that are mentioned as
@@ -14,20 +16,25 @@ public class Doc {
    * @return set of filenames that are mentioned as changed in the asciidoc step file
    */
   public static Set<String> filesChanged(String fname) {
-    // TODO: Implement this method
+    Set<String> changes = new HashSet<String>();
+
     try {
       BufferedReader reader = new BufferedReader(new FileReader(fname));
       String line;
+
       while((line = reader.readLine()) != null){
-        /*TODO: implement the following
-        Check for the WARNING: keyword at the beginning of the line. If the warning found then parse the file path
-        out of the line and add it the set
-         */
+        System.out.println(line);
+
+        String[] words = line.split("\\s");
+
+        if(words[0].equalsIgnoreCase(token)){
+          changes.add(words[1]);
+        }
       }
     } catch (Exception e) {
       System.err.format("Exception occurred trying to read '%s'.", fname);
       e.printStackTrace();
     }
-    throw new UnsupportedOperationException("Not implemented yet");
+    return changes;
   }
 }
