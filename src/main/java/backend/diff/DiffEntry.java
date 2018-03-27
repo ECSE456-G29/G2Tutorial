4752acb48785724ceb6dd0f59cce966ea267df78
Justin Bell <justin.bell@mail.mcgill.ca>
2018-03-27 15:06:19 -0400

    Fix liniting error

--- src/main/java/backend/diff/DiffEntry.java
+++ src/main/java/backend/diff/DiffEntry.java
@@ -43,12 +43,15 @@ public class DiffEntry {
 
   @Override
   public boolean equals(final Object obj) {
-    if (this == obj)
+    if (this == obj) {
       return true;
-    if (obj == null)
+    }
+    if (obj == null) {
       return false;
-    if (getClass() != obj.getClass())
+    }
+    if (getClass() != obj.getClass()) {
       return false;
+    }
 
     final DiffEntry other = (DiffEntry) obj;
     return this.hashCode() == other.hashCode();
